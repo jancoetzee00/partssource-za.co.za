42dc069337e25ab557cfb50028ea6a5342f2ee68
jancoetzee00 <jancoetzee00@gmail.com>
2026-08-16 18:34:17 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -9,8 +9,7 @@ import { PartListing, Seller } from "./src/types";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
-
+const PORT =  process.env.PORT || 3000;
 app.use(express.json());
 
 // In-memory Database for Part Listings
